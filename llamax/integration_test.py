7be85b5327f57aca45cd8b53044833606c0c0f76
Finbarr Timbers <finbarrtimbers@gmail.com>
2024-10-30 21:29:05 -0600

    Can generate proper text with argmax

--- llamax/integration_test.py
+++ llamax/integration_test.py
@@ -5,8 +5,12 @@ import flax.linen as nn
 import jax
 import jax.numpy as jnp
 import numpy as np
+import os
+
+import transformers
 import torch
 
+from llamax import generate
 from llamax import model
 from llamax import reference_model_torch
 
@@ -30,13 +34,14 @@ LLAMA_32_1B_CONFIG = llamax.ModelArgs(
 
 # This is the number of params in Llama 3.2 1B.
 NUM_WEIGHTS = 1_498_482_688
+MAX_LENGTH = 32
 
 # These are all length 128 after the prompt, and
 KNOWN_TEXT = {
-    "llama_3.2_1B": {
+    "meta-llama/Llama-3.1-8B": {
         "Hello, world!": (
             " I’m a 20-something year old who loves to write. I’m a huge fan "
-            "of the Harry Potter series, and I’m also a huge fan"
+            "of the Harry Potter series, and I’m also a huge fan of"
         ),
     }
 }
@@ -78,9 +83,16 @@ class IntegrationTests(unittest.TestCase):
         # Enable float64 precision in JAX
         jax.config.update("jax_enable_x64", True)
 
+        self.prompt = "Hello, world!"
+        self.model = "meta-llama/Llama-3.1-8B"
+        assert self.model in KNOWN_TEXT and self.prompt in KNOWN_TEXT[self.model]
+        self.tokenizer = transformers.AutoTokenizer.from_pretrained(
+            self.model, token=os.environ["HF_TOKEN"])
+
         # Set random seed for reproducibility
-        np.random.seed(42)
-        torch.manual_seed(42)
+        self.seed = 42
+        np.random.seed(self.seed)
+        torch.manual_seed(self.seed)
 
         self.config = LLAMA_32_1B_CONFIG
         # Ensure freqs computation is in float64
@@ -114,8 +126,8 @@ class IntegrationTests(unittest.TestCase):
         # Convert params to float64
         self.params = model.transformer_params_from_module(self.torch_model)
 
-        flax_model = model.Transformer(self.config)
-        self.apply_fn = jax.jit(flax_model.apply)
+        self.flax_model = model.Transformer(self.config)
+        self.apply_fn = jax.jit(self.flax_model.apply)
 
     def test_logits_match(self):
         inputs = np.random.randint(
@@ -133,6 +145,10 @@ class IntegrationTests(unittest.TestCase):
 
         flax_logits = self.apply_fn(self.params, inputs, start_pos=0, mask=mask)
 
+        torch_argmax = jnp.argmax(torch_logits, axis=-1)
+        flax_argmax = jnp.argmax(flax_logits, axis=-1)
+        np.testing.assert_array_equal(torch_argmax, flax_argmax)
+        
         # First, we check that both tensors have no nans/infs
         self.assertTrue(np.isfinite(torch_logits).all())
         self.assertTrue(np.isfinite(flax_logits).all())
@@ -140,7 +156,18 @@ class IntegrationTests(unittest.TestCase):
         # Fails at 6 decimal points with float64, passes at 5.
         np.testing.assert_array_almost_equal(torch_logits, flax_logits,
                                              decimal=5)
-                                             
+
+    def test_known_text_generation(self):
+        """Test that the model generates expected tokens for known prompts."""
+        text = generate.generate_text(
+            params=self.params,
+            model=self.flax_model,
+            tokenizer=self.tokenizer,
+            prompt=self.prompt,
+            max_length=32,
+            temperature=0.,
+            seed=self.seed)
+        self.assertEqual(text, self.prompt + KNOWN_TEXT[self.model][self.prompt])        
 
     @unittest.skip("This is slow so we don't run it by default.")
     def test_checkpoint_matches_torch(self):
